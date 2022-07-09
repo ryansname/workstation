3cb64c057f61ed8b7671b0218c95ecf3a6f4ecf2
Ryan Lewis <ryansname@gmail.com>
2022-07-09 15:43:56 +1200

    Working INGUI!

--- build.zig
+++ build.zig
@@ -1,4 +1,3 @@
-const glfw = @import("libs/mach-glfw/build.zig");
 const imgui = @import("libs/Zig-ImGui/zig-imgui/imgui_build.zig");
 const std = @import("std");
 
@@ -19,7 +18,7 @@ pub fn build(b: *std.build.Builder) !void {
 
     const exe = b.addExecutable("workstation", "src/main.zig");
 
-    addDeps(b, exe);
+    addDeps(exe, target);
 
     exe.setTarget(target);
     exe.setBuildMode(mode);
@@ -35,7 +34,7 @@ pub fn build(b: *std.build.Builder) !void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_tests = b.addTest("src/main.zig");
-    addDeps(b, exe_tests);
+    addDeps(exe_tests, target);
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
 
@@ -45,17 +44,37 @@ pub fn build(b: *std.build.Builder) !void {
     test_step.dependOn(&exe_tests.step);
 }
 
-fn addDeps(b: *std.build.Builder, target: *std.build.LibExeObjStep) void {
-    target.defineCMacro("GLFW_INCLUDE_NONE", null);
+fn addDeps(exe: *std.build.LibExeObjStep, target: std.zig.CrossTarget) void {
+    imgui.link(exe);
 
-    target.addPackagePath("zgl", "libs/zgl/zgl.zig");
-    target.linkSystemLibrary("epoxy");
+    const impl_glfw = .{ .name = "imgui_impl_glfw", .source = std.build.FileSource.relative("libs/Zig-ImGui/examples/imgui_impl_glfw.zig"), .dependencies = &.{imgui.pkg} };
+    const impl_opengl = .{ .name = "imgui_impl_opengl3", .source = std.build.FileSource.relative("libs/Zig-ImGui/examples/imgui_impl_opengl3.zig"), .dependencies = &.{imgui.pkg} };
+    exe.addPackage(impl_glfw);
+    exe.addPackage(impl_opengl);
+    exe.addPackagePath("gl", "libs/Zig-ImGui/examples/include/gl.zig");
+    exe.addPackagePath("glfw", "libs/Zig-ImGui/examples/include/glfw.zig");
+    exe.addSystemIncludePath("libs/Zig-ImGui/examples/include/c_include/");
 
-    target.addPackage(glfw.pkg);
-    glfw.link(b, target, .{});
+    linkGlad(exe, target);
+    linkGlfw(exe, target);
 
-    imgui.link(target);
+    exe.addLibPath("/opt/homebrew/lib/");
+    exe.addIncludeDir("/opt/homebrew/include/");
+}
 
-    target.addLibPath("/opt/homebrew/lib/");
-    target.addIncludeDir("/opt/homebrew/include/");
+fn linkGlad(exe: *std.build.LibExeObjStep, target: std.zig.CrossTarget) void {
+    _ = target;
+    exe.addIncludeDir("libs/Zig-ImGui/examples/include/c_include");
+    exe.addCSourceFile("libs/Zig-ImGui/examples/c_src/glad.c", &[_][]const u8{"-std=c99"});
+    //exe.linkSystemLibrary("opengl");
+}
+
+fn linkGlfw(exe: *std.build.LibExeObjStep, target: std.zig.CrossTarget) void {
+    if (target.isWindows()) {
+        exe.addObjectFile(if (target.getAbi() == .msvc) "libs/Zig-ImGui/examples/lib/win/glfw3.lib" else "libs/Zig-ImGui/examples/lib/win/libglfw3.a");
+        exe.linkSystemLibrary("gdi32");
+        exe.linkSystemLibrary("shell32");
+    } else {
+        exe.linkSystemLibrary("glfw");
+    }
 }
--- deps.sh
+++ deps.sh
@@ -3,11 +3,6 @@
 mkdir -p libs
 cd libs
 
-[ ! -d mach-glfw ] && git clone https://github.com/hexops/mach-glfw.git
-(cd mach-glfw && git fetch && git checkout 385f718)
-
 [ ! -d zig-imgui ] && git clone https://github.com/SpexGuy/Zig-ImGui.git
 (cd zig-imgui && git fetch && git checkout 0a2cfca)
 
-[ ! -d zgl ] && git clone https://github.com/ziglibs/zgl.git
-(cd zgl && git fetch && git checkout 32608da)
