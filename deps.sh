073db6c4c22c37812c4d2cbe48a1257c136e5342
Ryan Lewis <ryansname@gmail.com>
2023-06-19 10:01:03 +1200

    Fix capitalisation for zig-imgui

--- deps.sh
+++ deps.sh
@@ -3,8 +3,8 @@
 mkdir -p libs
 pushd libs
 
-[ ! -d zig-imgui ] && git clone https://github.com/SpexGuy/Zig-ImGui.git
-pushd zig-imgui
+[ ! -d Zig-ImGui ] && git clone https://github.com/SpexGuy/Zig-ImGui.git
+pushd Zig-ImGui
     git remote -v | grep ryansname || git remote add ryansname https://github.com/ryansname/Zig-ImGui.git
     git remote -v | grep michaelbartnett || git remote add michaelbartnett https://github.com/michaelbartnett/Zig-ImGui.git
     git fetch --all && git checkout 04f246a
